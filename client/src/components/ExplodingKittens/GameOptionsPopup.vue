722ab0328ef837f7b72f3c748a4354ea6124ce32
Andriy Blyschak <ably@ciklum.com>
2019-03-18 15:54:50 +0200

    [version] 0.9.1-b

--- client/src/components/ExplodingKittens/GameOptionsPopup.vue
+++ client/src/components/ExplodingKittens/GameOptionsPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="game-options-popup" @click="close">
+  <div class="game-options-popup" @click.self="close">
     <div class="game-options-popup__wrapper">
       <h1 class="game-options-popup__title">{{ $text('GAME_ROOMS.GAME_SETTINGS') }}</h1>
       <h3 class="game-options-popup__title">{{ $text('GAME_ROOMS.PACKS.LABEL') }}</h3>
